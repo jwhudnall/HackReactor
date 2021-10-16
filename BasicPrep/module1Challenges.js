d7fadc90ba43b6e0fe82c46828e1f6af39f6e78d
James Hudnall <jimmyhudnall@gmail.com>
2021-10-16 09:56:09 -0400

    Add solutions to Conditionals 1

---
+++ BasicPrep/module1Challenges.js
@@ -0,0 +1,18 @@
+// Conditionals 1
+function isOldEnoughToDrink(age) {
+    return age >= 21;
+  }
+
+  function isOldEnoughToDrive(age) {
+    return age >= 16;
+  }
+
+  function isOldEnoughToVote(age) {
+    return age>= 18;
+  }
+
+  function isOldEnoughToDrinkAndDrive(age) {
+    // Always illegal to drink and drive in US
+    return false;
+  }
+
