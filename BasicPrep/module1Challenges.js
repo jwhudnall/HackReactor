dde0bbc488c52a9612e07da3ff7d14267df30bb7
James Hudnall <jimmyhudnall@gmail.com>
2021-10-16 10:19:45 -0400

    Add solutions to String Methods 1

--- BasicPrep/module1Challenges.js
+++ BasicPrep/module1Challenges.js
@@ -28,3 +28,16 @@ function getProperty(obj, key) {
   function removeProperty(obj, key) {
     delete obj[key];
   }
+
+  // String Methods 1
+  function getFullName(firstName, lastName) {
+    return `${firstName} ${lastName}`;
+  }
+
+  function getLengthOfWord(word) {
+    return word.length;
+  }
+
+  function getLengthOfTwoWords(word1, word2) {
+    return word1.length + word2.length;
+  }
