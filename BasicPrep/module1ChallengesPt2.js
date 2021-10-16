c261363a5e48331af6f295d4adba7e071a13d819
James Hudnall <jimmyhudnall@gmail.com>
2021-10-16 14:37:43 -0400

    Add solutions to Array Methods 1 and 2

--- BasicPrep/module1ChallengesPt2.js
+++ BasicPrep/module1ChallengesPt2.js
@@ -14,3 +14,35 @@ function isPersonOldEnoughToDrinkAndDrive(person) {
 }
 
 // Objects 3
+function isPersonOldEnoughToDrive(person) {
+    return person.age >= 16;
+}
+
+function isPersonOldEnoughToVote(person) {
+    return person.age >= 18;
+}
+
+function addArrayProperty(obj, key, arr) {
+    // Bracket notation needed for "key" variable use
+    obj[key] = arr;
+}
+
+// Array Methods 1
+function getNthElement(array, n) {
+    return array[n];
+}
+
+function getFirstElement(array) {
+    return array[0];
+}
+
+function getLastElement(array) {
+    const lastIndex = array.length - 1;
+    return array[lastIndex];
+}
+
+// Array Methods 2
+function addToFront(arr, element) {
+    arr.unshift(element);
+    return arr;
+}
