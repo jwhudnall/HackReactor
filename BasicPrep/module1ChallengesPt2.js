c36412c0f8aaaeb05523a88c31f1ddf4632f52e7
James Hudnall <jimmyhudnall@gmail.com>
2021-10-16 14:31:25 -0400

    Add solutions to Objects 2 and 3

---
+++ BasicPrep/module1ChallengesPt2.js
@@ -0,0 +1,16 @@
+// Module 1 Challenges - Part II
+
+// Objects 2
+function addFullNameProperty(obj) {
+    obj['fullName'] = obj.firstName + ' ' + obj.lastName;
+}
+
+function addObjectProperty(obj1, key, obj2) {
+    obj1[key] = obj2;
+}
+
+function isPersonOldEnoughToDrinkAndDrive(person) {
+    return false;
+}
+
+// Objects 3
