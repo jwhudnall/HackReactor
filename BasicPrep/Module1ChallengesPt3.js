c79941f5ab0f9c18bd9c2dfde96fdb6747929615
James Hudnall <jimmyhudnall@gmail.com>
2021-10-17 06:59:19 -0400

    Add solutions for first half of Part 3 Exercises

---
+++ BasicPrep/Module1ChallengesPt3.js
@@ -0,0 +1,66 @@
+// Module 1, Part 3 Exercises
+
+// Array String Methods 1
+function getAllLetters(str) {
+    return str.split('');
+}
+
+function getAllWords(str) {
+    if (str === '') {
+        return [];
+    }
+    return str.split(' ');
+}
+
+// Advanced 1
+function countWords(str) {
+    if (str === '') {
+        return {}
+    }
+    count = {};
+    wordArray = str.split(' ');
+
+    for (word of wordArray) {
+        if (count[word] === undefined) {
+            count[word] = 1;
+        } else {
+            count[word]++;
+        }
+    }
+    return count;
+}
+
+// Conditionals 6
+function or(expression1, expression2) {
+    if (!expression1 && !expression2) {
+        return false;
+    }
+    return true;
+}
+//Advanced - Ternary Operator
+function or(expression1, expression2) {
+    return !expression1 && !expression2 ? false : true;
+}
+
+function isEitherEvenOrAreBoth7(num1, num2) {
+    const eitherEven = num1 % 2 === 0 || num2 % 2 === 0;
+    const both7 = num1 === 7 && num2 === 7;
+    return eitherEven || both7;
+}
+
+function isEitherEvenAndLessThan9(num1, num2) {
+    const eitherEven = num1 % 2 === 0 || num2 % 2 === 0;
+    const lessThan9 = num1 < 9 && num2 < 9;
+    return eitherEven && lessThan9;
+}
+
+// Advanced 2
+function extend(obj1, obj2) {
+    for (let key in obj2) {
+        if (obj1[key] === undefined) {
+            obj1[key] = obj2[key];
+        }
+    }
+}
+
+// Objects 6
