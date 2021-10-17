97c8060339fcd9d994b6ac1f279aec5c39db4208
James Hudnall <jimmyhudnall@gmail.com>
2021-10-17 09:39:25 -0400

    Add solutions for remainder of part 3

--- BasicPrep/Module1ChallengesPt3.js
+++ BasicPrep/Module1ChallengesPt3.js
@@ -114,3 +114,142 @@ function removeStringValuesLongerThan(num, obj) {
         }
     }
 }
+
+// Objects 5
+function removeEvenValues(obj) {
+    for (let key in obj) {
+        if (typeof obj[key] === 'number' && obj[key] % 2 === 0) {
+            delete obj[key];
+        }
+    }
+}
+
+function removeEvenValues(obj) {
+    for (let key in obj) {
+        if (typeof obj[key] === 'number' && obj[key] % 2 === 0) {
+            delete obj[key];
+        }
+    }
+}
+
+// Objects 5
+function countNumberOfKeys(obj) {
+    return Object.keys(obj).length;
+}
+
+function removeOddValues(obj) {
+    for (let key in obj) {
+        let value = obj[key];
+        if (typeof value === 'number' && value % 2 === 1) {
+            delete obj[key];
+        }
+    }
+}
+
+// Array String Methods 2
+function convertDoubleSpaceToSingle(str) {
+    // split string into array by ' ';
+    // join string using ' '
+    const strArray = str.split('  ');
+    return strArray.join(' ');
+}
+
+// Array Methods 7
+function joinThreeArrays(arr1, arr2, arr3) {
+    return arr1.concat(arr2).concat(arr3);
+}
+
+function addToFrontOfNew(arr, element) {
+    const newArr = [...arr];
+    newArr.unshift(element);
+    return newArr;
+}
+
+function addToBackOfNew(arr, element) {
+    const newArr = arr.slice();
+    newArr.push(element);
+    return newArr;
+}
+function getAllElementsButNth(array, n) {
+    const newArr = array.slice();
+    newArr.splice(n, 1);
+    return newArr;
+}
+
+// Iteration 1
+function getIndexOf(char, str) {
+    let index = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// Conditionals 7
+function areValidCredentials(name, password) {
+    const validName = name.length > 3;
+    const validPassword = password.length > 8;
+    return validName && validPassword;
+}
+
+function findMinLengthOfThreeWords(word1, word2, word3) {
+    let shortestWord = +Infinity;
+    const wordArr = [word1, word2, word3];
+    for (let word of wordArr) {
+        if (word.length < shortestWord) {
+            shortestWord = word.length;
+        }
+    }
+    return shortestWord;
+}
+
+function findMaxLengthOfThreeWords(word1, word2, word3) {
+    const wordArr = [word1, word2, word3];
+    let maxLen = -Infinity;
+
+    for (let word of wordArr) {
+        if (word.length > maxLen) {
+            maxLen = word.length;
+        }
+    }
+    return maxLen;
+}
+
+// Conditionals 9
+function getLongestOfThreeWords(word1, word2, word3) {
+    const wordArr = [word1, word2, word3];
+    let longestWord = '';
+    for (let word of wordArr) {
+        if (word.length > longestWord.length) {
+            longestWord = word;
+        }
+    }
+    return longestWord;
+}
+
+function findShortestOfThreeWords(word1, word2, word3) {
+    const wordArr = [word1, word2, word3];
+    let shortestWord = word1;
+    for (let i = 1; i < wordArr.length; i++) {
+        if (wordArr[i].length < shortestWord.length) {
+            shortestWord = wordArr[i];
+        }
+    }
+    return shortestWord;
+}
+
+// Advanced 3
+function select(arr, obj) {
+    const newObj = {};
+    // Iterate through arr
+    // If key exists in object, assign obj[key] to new obj
+    for (let i = 0; i < arr.length; i++) {
+        const arrVal = arr[i];
+        if (obj[arrVal] !== undefined) {
+            newObj[arrVal] = obj[arrVal];
+        }
+    }
+    return newObj;
+}
