4157a6f49f10b4ec821e8ee34144e50f9b6084eb
James Hudnall <jimmyhudnall@gmail.com>
2021-10-19 21:41:38 -0500

    Add additional solutions to Part 5 Exercises

--- BasicPrep/module1ChallengesPt5.js
+++ BasicPrep/module1ChallengesPt5.js
@@ -132,8 +132,81 @@ function getLargestNumberAmongMixedElements(arr) {
 // Iteration 3
 function computeSummationToN(n) {
     let total = 0;
-    for(let i = 1; i <= n; i++) {
+    for (let i = 1; i <= n; i++) {
         total += i;
     }
     return total;
-  }
+}
+
+// Conditionals 8
+function convertScoreToGrade(score) {
+    if (score >= 90 && score <= 100) {
+        return 'A';
+    } else if (score >= 80 && score <= 89) {
+        return 'B';
+    } else if (score >= 70 && score <= 79) {
+        return 'C';
+    } else if (score >= 60 && score <= 69) {
+        return 'D';
+    } else if (score >= 0 && score <= 59) {
+        return 'F';
+    } else {
+        return 'INVALID SCORE'
+    }
+}
+
+function convertScoreToGradeWithPlusAndMinus(score) {
+    let modifier = '';
+    if (score >= 60) {
+        if (score.toString().length === 2) {
+            let lastDigit = score.toString().slice(1);
+            if (lastDigit >= 0 && lastDigit <= 2) {
+                modifier = '-';
+            } else if (lastDigit >= 8 && lastDigit <= 9) {
+                modifier = '+';
+            }
+        } else if (score === 100) {
+            modifier = '+';
+        }
+    }
+    if (score >= 90 && score <= 100) {
+        return 'A' + modifier;
+    } else if (score >= 80 && score <= 89) {
+        return 'B' + modifier;
+    } else if (score >= 70 && score <= 79) {
+        return 'C' + modifier;
+    } else if (score >= 60 && score <= 69) {
+        return 'D' + modifier;
+    } else if (score >= 0 && score <= 59) {
+        return 'F';
+    } else {
+        return 'INVALID SCORE';
+    }
+}
+
+// Iteration 4
+function computeFactorialOfN(n) {
+    if (typeof n === 'number' && n > 0) {
+        let total = 1;
+        for (let i = n; i > 0; i--) {
+            total *= i;
+        }
+        return total;
+    }
+}
+
+// *** Recursive Solution ***
+function computeFactorialOfN(n) {
+    if (n === 0) {
+        return 1;
+    }
+    return n * computeFactorialOfN(n - 1);
+}
+
+function repeatString(string, num) {
+    let finalStr = '';
+    for (let i = num; i > 0; i--) {
+        finalStr += string;
+    }
+    return finalStr;
+}
