4c48cdfa6cf85df7febc7282ed1154373e0ce6bd
James Hudnall <jimmyhudnall@gmail.com>
2021-10-22 16:06:22 -0400

    Add solution for assertObjectsEqual

--- BasicPrep/Module2/testing.js
+++ BasicPrep/Module2/testing.js
@@ -13,14 +13,27 @@ function assertEqual(actual, expected, testName) {
 function assertArraysEqual(actual, expected, testName) {
     const sameLength = actual.length === expected.length;
     let sameValues = true;
-    for(let i = 0; i < actual.length; i++) {
-      if(actual[i] !== expected[i]) {
-        sameValues = false;
-      } 
+    for (let i = 0; i < actual.length; i++) {
+        if (actual[i] !== expected[i]) {
+            sameValues = false;
+        }
     }
-    if(sameLength && sameValues) {
-      console.log('passed');
+    if (sameLength && sameValues) {
+        console.log('passed');
     } else {
-      return console.log(`FAILED [${testName}] Expected "${expected}", but got "${actual}"`);
+        return console.log(`FAILED [${testName}] Expected "${expected}", but got "${actual}"`);
     }
-  }
+}
+
+// assertObjectsEqual
+function assertObjectsEqual(actual, expected, testName) {
+    const jsonActual = JSON.stringify(actual);
+    const jsonExpected = JSON.stringify(expected);
+
+    const identicalObjects = jsonActual === jsonExpected;
+    if (identicalObjects) {
+        console.log('passed');
+    } else {
+        console.log(`FAILED [${testName}] Expected ${jsonExpected}, but got ${jsonActual}`)
+    }
+}
