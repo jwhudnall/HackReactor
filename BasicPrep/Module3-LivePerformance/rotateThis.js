4c553f2c664cd2eef7d925f7d81c4b9938c7d0b3
James Hudnall <jimmyhudnall@gmail.com>
2021-11-01 06:17:54 -0400

    Add alternate solution for rotateThis

--- BasicPrep/Module3-LivePerformance/rotateThis.js
+++ BasicPrep/Module3-LivePerformance/rotateThis.js
@@ -42,3 +42,53 @@ var inputstr2_2 = 'lo worldhel';
 var expected1 = true;
 var actual1 = isRotated(inputstr1_2, inputstr2_2);
 assertEqual(expected1, actual1, 'Correctly identifies a rotated string');
+
+// ========== Alternate Version, without Hint ==========
+/* 
+Is one string a rotated version of another?
+
+For example:
+String 1: 'hello world'
+String 2: 'orldhello w'
+
+*/
+
+function isStillRotated(string1, string2) {
+    // declare target array of letters
+    var targetArr = string1.split('');
+    // declare working array of letters (copy of 1st)
+    var workingArr = string2.split('');
+    // iterate through all possible combinations (length of string1)
+    for(let i = 0; i < targetArr.length; i++) {
+      // rejoin target
+      var targetStr = targetArr.join('');
+      // rejoin current working
+      var workingStr = workingArr.join('');
+      // if these are equivalent, return true;
+      if(targetStr === workingStr) {
+        return true
+      } else {
+        var lastChar = workingArr.pop();
+        workingArr.unshift(lastChar);
+      }
+      // else
+        // capture popped value from working array
+        // unshift popped value to working array
+    }
+    // return false
+    return false;
+  }
+
+
+// Tests
+var inputstr1_1 = 'hello world';
+var inputstr2_1 = 'orldhello w';
+var expected1 = true;
+var actual1 = isStillRotated(inputstr1_1, inputstr2_1);
+assertEqual(expected1, actual1, 'Correctly identifies a rotated string');
+
+var inputstr1_2 = 'hello world';
+var inputstr2_2 = 'lo worldhel';
+var expected1 = true;
+var actual1 = isStillRotated(inputstr1_2, inputstr2_2);
+assertEqual(expected1, actual1, 'Correctly identifies a rotated string');
