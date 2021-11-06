367067ecdb86d56c565b8ba8634b55bdc459e475
James Hudnall <jimmyhudnall@gmail.com>
2021-11-06 12:08:51 -0400

    Add solution to detectNetwork

--- BasicPrep/Module3-LivePerformance/advancedPractice.js
+++ BasicPrep/Module3-LivePerformance/advancedPractice.js
@@ -49,5 +49,66 @@ function getLongestWordOfMixedElements(arr) {
     return longestWord;
 }
 
+/* 
+Problem 2 on Galvanize Website:
 
+Write the function detectNetwork. It should accept a string or a number for its cardNumber argument and, 
+based on the provided cardData, return the appropriate network string (or undefined if there's no match).
 
+var cardData = [
+  {
+    'issuer/network': 'Visa',  // card issuer (network)
+    prefixes: ['4'],	       // beginning digits
+    lengths: [13, 16, 19]      // lengths of card numbers
+  },
+  {
+    'issuer/network': 'Mastercard',
+    prefixes: ['51', '52', '53', '54', '55'],
+    lengths: [16]
+  },
+  {
+    'issuer/network': 'American Express',
+    prefixes: ['34', '37'],
+    lengths: [15]
+  },
+  {
+    'issuer/network': 'Diner\'s Club',
+    prefixes: ['38', '39'],
+    lengths: [14]
+  }
+];
+
+function detectNetwork(cardNumber, cardData) {
+  // your solution here
+}
+
+// example
+var network = detectNetwork('343456789012345', cardData);
+console.log(network); // --> 'American Express'
+*/
+
+function detectNetwork(cardNumber, cardData) {
+    // convert cardNumber to string;
+    var cardNumStr = cardNumber.toString();
+    var actualLength = cardNumStr.length;
+    for (let i = 0; i < cardData.length; i++) {
+        var issuer = cardData[i]['issuer/network'];
+        var prefixArr = cardData[i]['prefixes'];
+        var lengthArr = cardData[i]['lengths'];
+        var isValidLength = lengthArr.includes(actualLength);
+        var hasValidPrefix = validPrefix(cardNumStr, prefixArr);
+        if (isValidLength && hasValidPrefix) {
+            return issuer;
+        }
+    }
+    return undefined;
+}
+
+function validPrefix(str, prefixArr) {
+    for (let i = 0; i < prefixArr.length; i++) {
+        if (str.indexOf(prefixArr[i]) === 0) {
+            return true;
+        }
+    }
+    return false;
+}
