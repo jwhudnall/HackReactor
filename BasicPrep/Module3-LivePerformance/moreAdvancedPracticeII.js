ffef09f6cecfd6b4266dba139521a63d1e7b4ada
James Hudnall <jimmyhudnall@gmail.com>
2021-11-07 16:53:07 -0500

    Add solution to highestScoringWord()

--- BasicPrep/Module3-LivePerformance/moreAdvancedPracticeII.js
+++ BasicPrep/Module3-LivePerformance/moreAdvancedPracticeII.js
@@ -19,6 +19,48 @@ function splitPairs(str) {
 }
 
 
+/*
+===== Highest Scoring Word =====
+Given a string of words, you need to find the highest scoring word.
+
+Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+You need to return the highest scoring word as a string.
+
+If two words score the same, return the word that appears earliest in the original string.
+
+All letters will be lowercase and all inputs will be valid.
+*/
+function highestScoringWord(string) {
+    let highScore = 0;
+    let highScoreWord = '';
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const alphaArr = alphabet.split('');
+    const alphaScore = {};
+
+    for (let i = 0; i < alphaArr.length; i++) {
+        let char = alphaArr[i];
+        let letterScore = i + 1;
+        alphaScore[char] = letterScore;
+    }
+
+    // split string on spaces into wordArr
+    const wordArr = string.split(' ');
+    for (let i = 0; i < wordArr.length; i++) {
+        let word = wordArr[i];
+        let wordScore = 0;
+        for (let letter of word) {
+            wordScore += alphaScore[letter];
+        }
+        if (wordScore > highScore) {
+            highScore = wordScore;
+            highScoreWord = word;
+        }
+    }
+    return highScoreWord;
+}
+
+
 
 
 
