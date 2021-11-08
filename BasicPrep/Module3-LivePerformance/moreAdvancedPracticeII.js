1f6a3c24bc8b7bf800a517d0f608f243e1526540
James Hudnall <jimmyhudnall@gmail.com>
2021-11-08 06:31:12 -0500

    Add solution to generateSampleView()

--- BasicPrep/Module3-LivePerformance/moreAdvancedPracticeII.js
+++ BasicPrep/Module3-LivePerformance/moreAdvancedPracticeII.js
@@ -94,3 +94,107 @@ function getDomain(url) {
         }
     }
 }
+
+/* 
+===== generateSampleView() - Again =====
+
+*/
+function generateSampleView(objArr) {
+    const resultArr = [];
+    // examine each object within objArr
+    for (let i = 0; i < objArr.length; i++) {
+        // if id is odd
+        let userId = objArr[i].id;
+        if (userId % 2 === 1) {
+            // add users email to resultArr
+            let userEmail = objArr[i].email;
+            resultArr.push(userEmail);
+            console.log(`Email pushed to resultArr: ${userEmail}`);
+        } else {
+            let currentUser = objArr[i];
+            let userStreet = currentUser.address.street;
+            let userSuite = currentUser.address.suite;
+            let userCity = currentUser.address.city;
+            let userZip = currentUser.address.zipcode;
+            let addressStr = `${userStreet}, ${userSuite}, ${userCity}, ${userZip}`;
+            console.log(`Address pushed to resultArr: ${addressStr}`);
+            resultArr.push(addressStr);
+        }
+    }
+    return resultArr;
+}
+
+function assertArraysEqual(actual, expected, testName) {
+    // compare length
+    const sameLength = actual.length === expected.length;
+    if (!sameLength) {
+        console.log(`failed ${testName}. Arrays should have the same length`);
+        return;
+    }
+    for (let i = 0; i < actual.length; i++) {
+        if (actual[i] !== expected[i]) {
+            console.log(`failed ${testName}. Expected: ${expected} vs. Actual: ${actual}`);
+            return;
+        }
+    }
+    console.log('passed');
+}
+
+
+
+
+// Test Suite
+
+var users = [
+    {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+    {
+        "id": 2,
+        "name": "Ervin Howell",
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "address": {
+            "street": "Victor Plains",
+            "suite": "Suite 879",
+            "city": "Wisokyburgh",
+            "zipcode": "90566-7771",
+            "geo": {
+                "lat": "-43.9509",
+                "lng": "-34.4618"
+            }
+        },
+        "phone": "010-692-6593 x09125",
+        "website": "anastasia.net",
+        "company": {
+            "name": "Deckow-Crist",
+            "catchPhrase": "Proactive didactic contingency",
+            "bs": "synergize scalable supply-chains"
+        }
+    }
+];
+
+var actual = generateSampleView(users);
+var expected = ["Sincere@april.biz", "Victor Plains, Suite 879, Wisokyburgh, 90566-7771"];
+
+assertArraysEqual(actual, expected, 'Correctly parses object data as described');
