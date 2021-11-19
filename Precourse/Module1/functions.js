2690b7c6e69a98204bf6b1f550945bd74887211c
James Hudnall <jimmyhudnall@gmail.com>
2021-11-18 20:04:27 -0500

    Add dummy files to demonstrate CLI interaction

--- Precourse/Module1/functions.js
+++ Precourse/Module1/functions.js
@@ -13,9 +13,32 @@ for(var k in fun) {
 
 */
 
-var fun = function() {
-  console.log(2);
+// var fun = function() {
+//   console.log(2);
+// }
+
+// var res = fun();
+// console.log(res);
+
+// Parameters and Arguments
+var fun = function(input) {
+  console.log(arguments[0]);
+};
+
+setTimeout(function(){
+  fun('hi');
+}, 2000);
+
+var myFunc = function(arg1, arg2, arg3, arg4) {
+  console.log(arguments[2]);
+    return arguments[2];
+}
+myFunc('Hello', 'World');
+
+
+var myFunc = function(myStr) {
+  console.log('lamp shade');
+  return myStr;
 }
 
-var res = fun();
-console.log(res);
+setTimeout(myFunc('Hello World'), 1500);
