456af1497c818e4be3fe90d0cc05f18894d763a9
James Hudnall <jimmyhudnall@gmail.com>
2021-11-24 09:35:38 -0500

    Refactor to create and push a new stopwatch to the stopwatches array on a specified interval.
    Console reads out the status of the different stopwatches every n seconds

--- Precourse/Module2/closures.js
+++ Precourse/Module2/closures.js
@@ -1,30 +1,34 @@
-var makeStopwatch = function() {
+var makeStopwatch = function () {
   var elapsed = 0;
-  var stopwatch = function() {
+  var stopwatch = function () {
     return elapsed;
   };
-  var increase = function() { elapsed++ };
+  var increase = function () { elapsed++ };
   setInterval(increase, 1000);
-  console.log(stopwatch);
+  // console.log(stopwatch);
   return stopwatch;
 }
 
 var stopwatches = [];
 
-var stopwatch1 = makeStopwatch();
-stopwatches.push(stopwatch1);
+// var stopwatch1 = makeStopwatch();
+// stopwatches.push(stopwatch1);
 
-setTimeout(function() {
-  var stopwatch2 = makeStopwatch();
-  stopwatches.push(stopwatch2);
+setTimeout(function () {
+  setInterval(function () {
+    var newWatch = makeStopwatch();
+    stopwatches.push(newWatch);
+  }, 4000)
 }
-, 4000)
+  , 1000)
+
 
 // After 10 seconds, console should readout the status of all n stopwatches
 // within the stopwatches array
-setTimeout(function() {
+setInterval(function () {
   for (var i = 0; i < stopwatches.length; i++) {
     var currentWatch = stopwatches[i]();
-    console.log(`Stopwatch ${i+1}: ${currentWatch}`);
+    console.log(`Stopwatch ${i + 1}: ${currentWatch}`);
   }
+  console.log('________________________')
 }, 10000)
