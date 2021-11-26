ca8746b5c3ade484c5a364492c5a82e9e4a1c2cf
James Hudnall <jimmyhudnall@gmail.com>
2021-11-25 20:38:18 -0500

    Add _.memoize function

--- Precourse/Module2/closures2.js
+++ Precourse/Module2/closures2.js
@@ -12,3 +12,28 @@ _.delay = function(func, wait) {
     func.apply(null, args);
   }, wait);
 };
+
+// Memorize an expensive function's results by storing them.
+  // memoize could be renamed to oncePerUniqueArgumentList; memoize does the
+  // same thing as once, but based on many sets of unique arguments.
+  //
+  // _.memoize should return a function that, when called, will check if it has
+  // already computed the result FOR THE GIVEN ARGUMENT and return that value
+  // instead if possible.
+  _.memoize = function(func) {
+    var cache = {};
+    var result;
+
+    return function(n) {
+      var args = Array.prototype.slice.call(arguments).slice(1);
+      // check if every argument is in cache
+      if (args in cache) {
+        console.log('Found in cache!');
+        return cache[args];
+      } else {
+        result = func.apply(null, arguments);
+        cache[args] = result;
+        return result;
+      }
+    };
+  };
