64b313072d91b8d44923ffbab38e1a5ef54a6810
James Hudnall <jimmyhudnall@gmail.com>
2021-12-04 07:54:09 -0500

    Solve RPS using recursion (innerFunction method)

--- Recursion/recursionRPS.js
+++ Recursion/recursionRPS.js
@@ -16,3 +16,31 @@ var rockPaperScissors = function() {
   return result;
 }
 console.log(rockPaperScissors().length);
+
+
+// Recursive Solution (innerFunction method)
+var rockPaperScissors2 = function(rounds) {
+  // Declare result variable
+  var choices = ['rock', 'paper', 'scissors'];
+  var result = [];
+
+  // create innerfunction
+  var innerFunction = function(roundsToGo, choicesSoFar) {
+    // Base Case
+    if (roundsToGo === 0) {
+      result.push(choicesSoFar);
+      return;
+    }
+    // Recursive case
+    choices.forEach(function(choice) {
+      innerFunction(roundsToGo - 1, choicesSoFar.concat(choice));
+    });
+  };
+  innerFunction(rounds, []);
+
+  return result;
+}
+
+var result = rockPaperScissors2(3);
+
+console.log(result);
