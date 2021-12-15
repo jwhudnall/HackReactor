cdc74e274f98a210a1747de5dde058b777a94766
James Hudnall <jimmyhudnall@gmail.com>
2021-12-15 06:21:50 -0500

    Implement .first, .last, .each

---
+++ Precourse/Module1/underbar.js
@@ -0,0 +1,49 @@
+(function () {
+  'use strict';
+
+  window._ = {};
+
+  _.identity = function (val) {
+    return val;
+  };
+
+  // Return an array of the first n elements of an array. If n is undefined,
+  // return just the first element.
+  _.first = function (array, n) {
+    return n === undefined ? array[0] : array.slice(0, n);
+  };
+
+  // Like first, but for the last n elements. If n is undefined, return just the
+  // last element.
+  _.last = function (array, n) {
+    if (n > array.length) {
+      var startIdx = 0;
+    } else {
+      var startIdx = array.length - n;
+    }
+    var lastIdx = array.length - 1;
+
+    return n === undefined ? array[lastIdx] : array.slice(startIdx, lastIdx + 1);
+  };
+
+  // Call iterator(value, key, collection) for each element of collection.
+  // Accepts both arrays and objects.
+  //
+  // Note: _.each does not have a return value, but rather simply runs the
+  // iterator function over each item in the input collection.
+  _.each = function (collection, iterator) {
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
+  };
+
+
+
+
+}());
