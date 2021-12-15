1865e2da3cbf60bca4a6eece6403e8af73b4c7b0
James Hudnall <jimmyhudnall@gmail.com>
2021-12-15 06:30:35 -0500

    Implement filter, reject

--- Precourse/Module1/underbar.js
+++ Precourse/Module1/underbar.js
@@ -43,6 +43,27 @@
     }
   };
 
+  // Return all elements of an array that pass a truth test.
+  _.filter = function (collection, test) {
+    var results = [];
+
+    _.each(collection, function (element) {
+      if (test(element)) {
+        results.push(element);
+      }
+    });
+
+    return results;
+  };
+
+  // Return all elements of an array that don't pass a truth test.
+  _.reject = function (collection, test) {
+    // TIP: see if you can re-use _.filter() here, without simply
+    // copying code in and modifying it
+    return _.filter(collection, function (item) {
+      return !test(item);
+    });
+  };
 
 
 
